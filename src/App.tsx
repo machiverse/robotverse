7411d583f63be0b6e17fc7e9542c394192ffd0ff
Mani <mark.it@keyleerkorb.com>
2025-08-21 16:02:09 +0530

    tes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import DashboardPage from "./pages/DashboardPage";
 import SparePartsSellerDashboard from "./pages/SparePartsSellerDashboard";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(); 
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
